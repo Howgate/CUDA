bb61fab060907889d912c0b6998f97736dd808de
Howgate <jlhowgate@gmail.com>
2025-06-07 01:31:06 +0100

    Update impala.lua

--- impala.lua
+++ impala.lua
@@ -1,3 +1,3 @@
 while true do
-  shell.run("speaker play https://github.com/Howgate/CUDA/raw/refs/heads/main/tame.dfpwm")
+  shell.run("speaker play https://github.com/Howgate/CUDA/raw/refs/heads/main/Tame%20Impala%20Playlist.dfpwm")
 end
